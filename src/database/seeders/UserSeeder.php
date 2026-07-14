a8ef604ac59499005b334d19115bf0f59fa2062a
Muhammad Naufal Febrian <pallfebrian9@gmail.com>
2026-07-15 06:08:04 +0700

    Tambahkan seeder evaluasi dan amankan akun default

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -3,27 +3,36 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $user = User::firstOrCreate(
+        $admin = User::firstOrCreate(
             ['email' => 'admin@admin.com'],
-            ['name' => 'Super Admin', 'password' => Hash::make('password')]
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make(
+                    env('ADMIN_SEED_PASSWORD', Str::random(40))
+                ),
+            ]
         );
-        $user->assignRole('super_admin');
+
+        $admin->syncRoles(['super_admin']);
 
         $user = User::firstOrCreate(
             ['email' => 'user@admin.com'],
-            ['name' => 'User Account', 'password' => Hash::make('password')]
+            [
+                'name' => 'User Account',
+                'password' => Hash::make(
+                    env('USER_SEED_PASSWORD', Str::random(40))
+                ),
+            ]
         );
-        $user->assignRole('user');
+
+        $user->syncRoles(['user']);
     }
 }
